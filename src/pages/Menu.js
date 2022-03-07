eafb029f0896b1808f99318cb65dd41cacc594ca
Gjorgjioska <marina.gorgioska@gmail.com>
2022-03-07 20:46:40 +0100

    Add some css  MenuStyle

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,14 +1,22 @@
 import React from "react";
 import { MenuList } from "../helpers/MenuList";
+import MenuItem from "../components/MenuItem";
+import "../styles/Menu.css";
 
 const Menu = () => {
   return (
     <div className="menu">
       <h1 className="menuTitle">Our Menu</h1>
-      <div className="menuList">
-        {" "}
+      <div className="MenuList">
         {MenuList.map((menuItem, key) => {
-          return <div>{menuItem.name}</div>;
+          return (
+            <MenuItem
+              key={key}
+              image={menuItem.image}
+              name={menuItem.name}
+              price={menuItem.price}
+            />
+          );
         })}
       </div>
     </div>
