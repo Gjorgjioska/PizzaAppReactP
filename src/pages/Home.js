6f723f44a6637a2dd2b32ad58010c49eee62ec32
Gjorgjioska <marina.gorgioska@gmail.com>
2022-03-07 15:45:13 +0100

    Working on a home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,23 @@
+import { Button } from "@mui/material";
 import React from "react";
+import { Link } from "react-router-dom";
+import BannerImage from "../assets/pizza.jpg.jpg";
+import "../pages/Home.css";
 
 const Home = () => {
   return (
     <div>
-      <h1>Hello World</h1>
+      <div className="home"> </div>
+      <div
+        className="headerContainer"
+        style={{ backgroundImage: `url(${BannerImage})` }}
+      >
+        <h1>Marina's Pizzeria</h1>
+        <p>PIZZA TO FIT ANY TASTE</p>
+        <Link to="/menu">
+          <Button>ORDER NOW</Button>
+        </Link>
+      </div>
     </div>
   );
 };
