8b5a3ebe093ce9996a5dc513c94d834befb60ddb
Gjorgjioska <marina.gorgioska@gmail.com>
2022-03-07 20:09:12 +0100

    Working on Menu page, add some pizza recipe

---
+++ src/components/helpers/MenuList.js
@@ -0,0 +1,37 @@
+import Pepperoni from "../assets/Pepperoni.jpg";
+import Margherita from "../assets/Margherita.jpg";
+import MarinaTechSpecial from "../assets/MarinaTechSpecial.jpg";
+import Vegan from "../assets/Vegan.jpg";
+import Pineapple from "../assets/Pineapple.jpg";
+
+export const MenuList = [
+  {
+    name: "Pepperoni Pizza",
+    image: Pepperoni,
+    price: 12.99,
+  },
+
+  {
+    name: "Margherita Pizza",
+    image: Margherita,
+    price: 11.99,
+  },
+
+  {
+    name: "MarinaTech Special Pizza",
+    image: MarinaTechSpecial,
+    price: 217.56,
+  },
+
+  {
+    name: "Vegan Pizza",
+    image: Vegan,
+    price: 17.99,
+  },
+
+  {
+    name: "Pineapple Pizza",
+    image: Pineapple,
+    price: 7.99,
+  },
+];
---
+++ src/pages/Menu.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { MenuList } from "../helpers/MenuList";
+
+const Menu = () => {
+  return (
+    <div className="menu">
+      <h1 className="menuTitle">Our Menu</h1>
+      <div className="menuList">
+        {" "}
+        {MenuList.map((menuItem, key) => {
+          return <div>{menuItem.name}</div>;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
