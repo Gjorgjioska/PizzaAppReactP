a8ce328015d15fd4f25d6110596ffad1c7e15cdf
Gjorgjioska <marina.gorgioska@gmail.com>
2022-03-07 14:11:15 +0100

    Add some styling on navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>Pizaa Orders</h1>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
